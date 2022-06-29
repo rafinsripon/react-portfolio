18ea9ce791c5eba451d1a837fc61c0406638d5aa
Rafins_Ripon <rafinsripon@gmail.com>
2022-06-29 23:20:09 +0600

    updated anchor tah

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -66,9 +66,9 @@ function About() {
                     </div>
                 </div>
             </div>
-            {/* up_to_btn */}
+            {/* up_to_btn https: */}
             <div className="up_to_top">
-                <a href="https://" className='bottom_to_top'>
+                <a href="!#" className='bottom_to_top'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chevron-up white" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
                 </svg>
